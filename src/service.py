1e9112e9fb1d0c38b2127f58230a85973b9da813
miquelduranfrigola <miquelduranfrigola@gmail.com>
2021-10-19 17:53:04 +0200

    bugfix filename

--- src/service.py
+++ src/service.py
@@ -51,7 +51,7 @@ class Model(object):
         run_file = os.path.join(tmp_folder, self.RUN_FILE)
         with open(run_file, "w") as f:
             lines = [
-                "python {0}/predict {1} {2}".format(
+                "python {0}/predict.py {1} {2}".format(
                     self.framework_dir,
                     data_file,
                     pred_file
